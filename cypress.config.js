41b94a124d352922badb908511b5ab9d1be9c83d
smf1995 <smf_1995@hotmail.com>
2023-02-02 01:05:01 +0000

    Add report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Hand-Picked combination',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    reporter: 'mochawesome',
   },
+  
 });
